b026f2d610c5176660c1df515379064cb513936d
imranm <imranm@alohatechnology.com>
2017-11-30 20:42:32 +0530

    Dashboard component unit test fix

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 import { DashboardComponent } from './dashboard.component';
+import { FriendSearchComponent } from '../friend-search/friend-search.component';
+import { FriendService } from '../friend.service';
+import { MessageService } from '../message.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +14,17 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ FriendSearchComponent, DashboardComponent ],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        FriendService,
+        HttpClient,
+        HttpHandler,
+        MessageService,
+      ]
     })
     .compileComponents();
   }));
