52b77bc631bbdeac3a65199412ca36adea7b656e
imranm <imranm@alohatechnology.com>
2017-11-30 20:35:40 +0530

    Friend service unit test case

--- src/app/friend.service.spec.ts
+++ src/app/friend.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 import { FriendService } from './friend.service';
+import { MessageService } from './message.service';
 
 describe('FriendService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FriendService]
+      providers: [ HttpClient, HttpHandler, MessageService, FriendService],
+
     });
   });
 
