48714aaa670118471801fe9a8e161290300dd576
imranm <imranm@alohatechnology.com>
2017-11-30 20:52:29 +0530

    Friend search unit test fix

--- src/app/friend-search/friend-search.component.spec.ts
+++ src/app/friend-search/friend-search.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { FriendSearchComponent } from './friend-search.component';
+import { FriendService } from '../friend.service';
+import { MessageService } from '../message.service';
 
 describe('FriendSearchComponent', () => {
   let component: FriendSearchComponent;
@@ -8,7 +12,9 @@ describe('FriendSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FriendSearchComponent ]
+      declarations: [ FriendSearchComponent ],
+      imports: [ RouterModule ],
+      providers: [ HttpClient, HttpHandler, FriendService, MessageService ]
     })
     .compileComponents();
   }));
@@ -22,4 +28,5 @@ describe('FriendSearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
